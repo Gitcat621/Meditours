74a9150105cab42432ce9e40f77ea12d87c36060
Alejandro <toothyx@hotmail.com>
2022-11-13 19:39:41 -0500

    Avance 1

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -32,6 +32,44 @@ namespace Meditours.Controllers
             return View();
         }
 
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Registrar()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CrearUsuario(usuario request)
+        {
+            try
+            {
+                if (request != null)
+                {
+                    usuario usuario = new usuario();
+                    usuario.Nombre = request.Nombre;
+                    usuario.User = request.User;
+                    usuario.password = request.password;
+                    usuario.FkRol = 1;
+
+                    _context.usuarios.Add(usuario);
+                    await _context.SaveChangesAsync();
+
+                    return RedirectToAction(nameof(Index));
+                }
+                return View();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Errors papu " + ex.Message);
+            }
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
