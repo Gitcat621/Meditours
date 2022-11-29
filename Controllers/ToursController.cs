55b09a37f33578e56e473faced59a25bc7d83dc3
Angel5764 <116742740+Angel5764@users.noreply.github.com>
2022-11-29 14:00:21 -0500

    Vista de destinos

---
+++ Controllers/ToursController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Meditours.Context;
+using Microsoft.Extensions.Logging;
+
+namespace Meditours.Controllers
+{
+    public class ToursController : Controller
+    {
+        private readonly ILogger<ToursController> _logger;
+        private readonly ApplicationDbContext _context;
+
+        public ToursController(ILogger<ToursController> logger, ApplicationDbContext context)
+        {
+            _logger = logger;
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var response = await _context.Destinos.ToListAsync();
+
+            return View(response);
+        }
+    }
+}
